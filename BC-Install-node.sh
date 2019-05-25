4b7fdd4474b1a836c87f67a06a818dd45b24c562
thesolarminer <thesolarminer@users.noreply.github.com>
2019-05-24 21:39:22 -0500

    1.0.2 Update

--- BC-Install-node.sh
+++ BC-Install-node.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Downloading the wallet
 echo 'Downloading and extract wallet files'
-wget "https://github.com/Bitcoin-Confidential/BitcoinC-Core/releases/download/v1.0.1/bitcoinc-1.0.1-x86_64-linux-gnu.tar.gz" -O - | tar -xz
+wget "https://github.com/Bitcoin-Confidential/BitcoinC-Core/releases/download/v1.0.2/bitcoinc-1.0.2-x86_64-linux-gnu.tar.gz" -O - | tar -xz
 
 # Extract the files and give executable permissions
 echo 'Copying files to home directory'
-cp bitcoinc-1.0.1/bin/bitcoincd ~/ 
-cp bitcoinc-1.0.1/bin/bitcoinc-cli ~/
+cp bitcoinc-1.0.2/bin/bitcoincd ~/ 
+cp bitcoinc-1.0.2/bin/bitcoinc-cli ~/
 chmod a+x ~/bitcoincd ~/bitcoinc-cli
 
 # Create wallet directory and set to run when windows is closed.
@@ -26,8 +26,8 @@ echo 'Enabling staking.'
 
 # Create a shortcut on system - Unhash if desired
 #echo 'Creating symbol link'
-#ln -sf ~/bitcoinc-1.0.1/bin/bitcoincd /usr/bin/bitcoincd
-#ln -sf ~/bitcoinc-1.0.1/bin/bitcoinc-cli /usr/bin/bitcoinc-cli
+#ln -sf ~/bitcoinc-1.0.2/bin/bitcoincd /usr/bin/bitcoincd
+#ln -sf ~/bitcoinc-1.0.2/bin/bitcoinc-cli /usr/bin/bitcoinc-cli
 
 echo 'Installation finished.'
 echo 'To stop wallet ./bitcoinc-cli stop'
